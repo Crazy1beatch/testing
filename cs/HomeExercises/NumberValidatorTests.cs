7b90a65f9a9ee57e354d02e2dad171ab6b0bcdf1
Crazy1beatch <zhora.shevelev196@mail.ru>
2023-12-01 14:00:56 +0500

    переделал домашку 2.0

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -8,16 +7,6 @@ namespace HomeExercises
 	[TestFixture]
 	public class NumberValidatorTests
 	{
-		private static NumberValidator numberValidatorOnlyPositive = null!;
-		private static NumberValidator numberValidatorNegative = null!;
-
-		[SetUp]
-		public void CreateNumberValidators()
-		{
-			numberValidatorOnlyPositive = new NumberValidator(5, 2, true);
-			numberValidatorNegative = new NumberValidator(5, 2);
-		}
-
 		[TestCase(1, 0, TestName = "Small_PrecisionAndScale")]
 		[TestCase(99999, 99998, TestName = "Big_PrecisionAndScale")]
 		public void NumberValidatorCreatesCorrectly_NoOnlyPositive(int precision, int scale)
@@ -30,7 +19,7 @@ namespace HomeExercises
 		[TestCase(1, 0, true, TestName = "Small_PrecisionAndScale_OnlyPositive_True")]
 		[TestCase(99999, 99998, false, TestName = "Big_PrecisionAndScale_OnlyPositive_False")]
 		[TestCase(99999, 99998, true, TestName = "Big_PrecisionAndScale_OnlyPositive_True")]
-		public void NumberValidatorCreatesCorrectly_WithValidArguments(int precision, int scale, bool onlyPositive)
+		public void NumberValidatorCreatesCorrectly_WhenValidArguments(int precision, int scale, bool onlyPositive)
 		{
 			var action = new Action(() => new NumberValidator(precision, scale, onlyPositive));
 			action.Should().NotThrow();
@@ -41,32 +30,24 @@ namespace HomeExercises
 		[TestCase(1, -1, TestName = "Negative_Scale")]
 		[TestCase(2, 3, TestName = "Scale_Grater_Than_Precision")]
 		[TestCase(2, 2, TestName = "Scale_Equals_To_Precision")]
-		public void ThrowException_WithInvalidArguments(int precision, int scale)
+		public void ThrowException_WhenInvalidArguments(int precision, int scale)
 		{
 			var action = new Action(() => new NumberValidator(precision, scale, true));
 			action.Should().Throw<ArgumentException>();
 		}
 
-		[TestCase("1.62", true, TestName = "No_Sign_Number_True")]
-		[TestCase("1,62", true, TestName = "No_Sign_Number_With_Coma_True")]
-		[TestCase("+64.2", true, TestName = "Positive_Sign_Number_True")]
-		[TestCase("+64,2", true, TestName = "Positive_Sign_Number_With_Coma_True")]
-		[TestCase("-12.13", false, TestName = "Negative_Sign_Number_False")]
-		[TestCase("-12,13", false, TestName = "Negative_Sign_Number_With_Coma_False")]
-		public void ValidatesCorrectly_WithOnlyPositive(string number, bool expected)
+		[TestCaseSource(typeof(TestData), nameof(TestData.TestCasesNumberValidation), new object []{true})]
+		public void ValidatesCorrectly_When_OnlyPositive(string number, bool expected)
 		{
-			numberValidatorOnlyPositive.IsValidNumber(number).Should().Be(expected);
+			var numberValidator = new NumberValidator(5, 2, true);
+			numberValidator.IsValidNumber(number).Should().Be(expected);
 		}
 
-		[TestCase("1.62", true, TestName = "No_Sign_Number_True")]
-		[TestCase("1,62", true, TestName = "No_Sign_Number_With_Coma_True")]
-		[TestCase("+64.2", true,  TestName = "Positive_Sign_Number_True")]
-		[TestCase("+64,2", true,  TestName = "Positive_Sign_Number_With_Coma_True")]
-		[TestCase("-12.13", true, TestName = "Negative_Sign_Number_True")]
-		[TestCase("-12,13", true, TestName = "Negative_Sign_Number_With_Coma_True")]
-		public void ValidatesCorrectly_WithNegative(string number, bool expected)
+		[TestCaseSource(typeof(TestData), nameof(TestData.TestCasesNumberValidation), new object []{false})]
+		public void ValidatesCorrectly_When_Negative(string number, bool expected)
 		{
-			numberValidatorNegative.IsValidNumber(number).Should().Be(expected);
+			var numberValidator = new NumberValidator(5, 2);
+			numberValidator.IsValidNumber(number).Should().Be(expected);
 		}
 
 		[TestCase("a.sd", TestName = "Not_A_Number")]
@@ -84,59 +65,13 @@ namespace HomeExercises
 		[TestCase("++0.1", TestName = "Too_Much_Signs")]
 		[TestCase("0.112", TestName = "Frac_Is_Bigger_Than_Scale")]
 		[TestCase("1110.11", TestName = "Number_Is_Bigger_Than_Precision")]
-		[TestCase("+110.11", TestName = "Number_Is_Bigger_Than_Precision_With_Sign")]
-		public void ReturnsFalse_With(string number)
+		[TestCase("+110.11", TestName = "Number_Is_Bigger_Than_Precision_When_Sign")]
+		public void ReturnsFalse_When(string number)
 		{
+			var numberValidatorOnlyPositive = new NumberValidator(5, 2, true);
+			var numberValidatorNegative = new NumberValidator(5, 2);
 			numberValidatorOnlyPositive.IsValidNumber(number).Should().Be(false);
 			numberValidatorNegative.IsValidNumber(number).Should().Be(false);
-		}
-	}
-
-	public class NumberValidator
-	{
-		private readonly Regex numberRegex;
-		private readonly bool onlyPositive;
-		private readonly int precision;
-		private readonly int scale;
-
-		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
-		{
-			this.precision = precision;
-			this.scale = scale;
-			this.onlyPositive = onlyPositive;
-			if (precision <= 0)
-				throw new ArgumentException("precision must be a positive number");
-			if (scale < 0 || scale >= precision)
-				throw new ArgumentException("scale must be a non-negative number less or equal than precision");
-			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
-		}
-
-		public bool IsValidNumber(string value)
-		{
-			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
-			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
-			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
-			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
-			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
-
-			if (string.IsNullOrEmpty(value))
-				return false;
-
-			var match = numberRegex.Match(value);
-			if (!match.Success)
-				return false;
-
-			// Знак и целая часть
-			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
-			// Дробная часть
-			var fracPart = match.Groups[4].Value.Length;
-
-			if (intPart + fracPart > precision || fracPart > scale)
-				return false;
-
-			if (onlyPositive && match.Groups[1].Value == "-")
-				return false;
-			return true;
 		}
 	}
 }
---
+++ cs/HomeExercises/TestData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace HomeExercises
+{
+	public class TestData
+	{
+		public static IEnumerable TestCasesNumberValidation(bool isOnlyPositive)
+		{
+			yield return new TestCaseData("1.62", true).SetName("No_Sign_Number_True");
+			yield return  new TestCaseData("1,62", true).SetName("No_Sign_Number_True_When_Coma");
+			yield return new TestCaseData("+64.2", true).SetName("Positive_Sign_Number_True");
+			yield return new TestCaseData("+64,2", true).SetName("Positive_Sign_Number_When_Coma_True");
+			yield return new TestCaseData("-12.13", !isOnlyPositive).SetName("Negative_Sign_Number_False");
+			yield return new TestCaseData("-12,13", !isOnlyPositive).SetName("Negative_Sign_Number_When_Coma_False");
+		}
+	}
+}
